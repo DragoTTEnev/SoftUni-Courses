17d5c9a3fdd49c3a52c942f0689b0b220124065c
DragoTTEnev <133972876+DragoTTEnev@users.noreply.github.com>
2023-05-18 15:49:51 +0300

    added files to see how commit works

---
+++ solution.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace WhatsGoingOn
+{
+    internal class Program
+    {
+        static void NoIdeaWhatImDoing(int a, int b)
+        {
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+        }
+    }
+}
